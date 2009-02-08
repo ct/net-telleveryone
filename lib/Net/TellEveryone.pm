4e587c546b5119dec653ffcd4763f92ade3ac46f
Chris Thompson <chris@cthompson.com>
2009-02-07 20:28:03 -0500

    Initial skeleton import.

---
+++ lib/Net/TellEveryone.pm
@@ -0,0 +1,42 @@
+##############################################################################
+# Net::TellEveryone - Central API to many status posting services
+# v2.06
+# Copyright (c) 2009 Screaming Mongoose, LLC
+##############################################################################
+
+package Net::TellEveryone;
+
+use Carp;
+use strict;
+
+use Moose;
+our $VERSION = '0.01';
+
+has user => (
+    isa => 'Str',
+    is  => 'rw',
+    default =>  sub { 1 }, 
+);
+
+has pass => (
+    isa => 'Str',
+    is  => 'rw',
+    default =>  sub { 1 }, 
+);
+
+has message => (
+    isa => 'Str',
+    is  => 'rw',
+    default =>  sub { 1 }, 
+);
+
+has ref_url => (
+    isa => 'Str',
+    is  => 'rw',
+    default =>  sub { 1 }, 
+);
+
+
+
+
+1; # End of Net::TellEveryone
