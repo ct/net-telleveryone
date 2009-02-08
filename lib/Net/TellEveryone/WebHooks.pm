101279870246f8648bd448f149a406ec58b172e6
Chris Thompson <chris@cthompson.com>
2009-02-08 15:18:39 -0500

    Modify WebHooks for streamlining and new payload format

--- lib/Net/TellEveryone/WebHooks.pm
+++ lib/Net/TellEveryone/WebHooks.pm
@@ -4,7 +4,7 @@
 # Copyright (c) 2009 Screaming Mongoose, LLC
 ##############################################################################
 
-package Net::TellEveryone::Webhooks;
+package Net::TellEveryone::WebHooks;
 
 use Carp;
 use strict;
@@ -16,56 +16,32 @@ use JSON::Any;
 
 our $VERSION = '1.00';
 
-subtype 'JSON' => as 'Str';
-
-coerce 'JSON' => from 'HashRef' => via {
-    JSON::Any->objToJson($_);
-};
-
-has url => (
-    isa     => 'Str',
-    is      => 'rw',
-    default => sub { '' },
-);
-
 has payload => (
-    isa     => 'JSON',
-    coerce  => 1,
-    is      => 'rw',
-    default => sub { '{ "payload": "0"}' },
-);
-
-has agent => (
-    isa     => 'Str',
-    is      => 'rw',
+    isa       => 'HashRef',
+    is        => 'rw',
+    lazy      => 1,
+    default   => sub { {} },
+    predicate => "has_payload",
 );
 
 has nte_object => (
-    isa => 'Net::TellEveryone',
-    is  => 'ro',
+    isa     => 'Net::TellEveryone',
+    is      => 'ro',
     require => 1,
     weaken =>
 );
 
 sub process {
     my $self = shift;
-    
-    carp 'process';
 
     my $ua = LWP::UserAgent->new();
-    $ua->agent( $self->agent );
+    $ua->agent( $self->nte_object->agent );
     $ua->env_proxy;
+    
+    my $args = { "payload" => JSON::Any->objToJson( $self->payload->{content} ) };
+    my $url = $self->payload->{url};
 
-    my $req = $ua->post(
-        $self->url,
-        {
-            payload => $self->payload,
-            user    => $self->nte_object->user,
-            message => $self->nte_object->message,
-            ref_url => $self->nte_object->ref_url,
-        }
-    );
-
+    my $req = $ua->post( $url, $args );
 }
 
 1;
