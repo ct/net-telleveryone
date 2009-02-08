91910ee3521489ba36fdbebcd38419c80936662a
Chris Thompson <chris@cthompson.com>
2009-02-07 21:15:27 -0500

    Add beginning logic.

--- lib/Net/TellEveryone/Webhooks.pm
+++ lib/Net/TellEveryone/Webhooks.pm
@@ -4,10 +4,50 @@
 # Copyright (c) 2009 Screaming Mongoose, LLC
 ##############################################################################
 
-package Net::TellEveryone;
+package Net::TellEveryone::Webhooks;
 
 use Carp;
 use strict;
 
 use Moose;
-our $VERSION = '0.01';
+use Moose::Util::TypeConstraints;
+use LWP::Useragent;
+use JSON::Any;
+
+our $VERSION = '1.00';
+
+subtype 'JSON' => as 'Str';
+
+coerce 'JSON'
+    => from 'HashRef'
+        => via { JSON::Any->objToJson($_) };
+
+has url => (
+    isa => 'Str',
+    is  => 'rw',    
+    default =>  sub { '' }, 
+);
+
+has payload => (
+    isa => 'JSON',
+    coerce => 1,
+    is  => 'rw',    
+    default =>  sub { '{ "payload": "0"}' }, 
+);
+
+has agent => (
+    isa => 'Str',
+    is  => 'rw',    
+    default =>  sub { "Net::TellEveryon/$Net::TellEveryone::VERSION (PERL)" }, 
+);
+
+sub process {
+    my $self = shift;
+    
+    my $ua = LWP::UserAgent->new();
+    $ua->agent( $self->agent );
+    $ua->env_proxy;
+    my $req = $ua->post( $self->url, { payload => $self->payload } );
+    
+}
+
